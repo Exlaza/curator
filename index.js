94ae5cd612dfdc3b64f8fb60fa07c82472bc279a
Ekansh <ekansh.jain15@gmail.com>
2018-09-24 23:42:22 +0530

    feat: boilerplate

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+	return 	<div>Hello React!</div>;
+}
+
+ReactDOM.render(<Index />, documnet.getElementByID("index"));
+
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+	template: "./src/index.html",
+	filename: "./index.html"
+});
+
+module.exports = {
+	entry: "./src/index.js",
+	output:{
+		path.join(__dirname, "/dist"),
+		filename: "index_bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			}
+		]
+	},
+	plugins: [htmlPlugin]
+};
+
